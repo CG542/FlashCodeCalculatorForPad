5ef9476b036f310faf5ff91b0f079a18bc16ec7e
Andy <Andy@Andys-MacBook-Air.local>
2015-11-04 22:39:00 +0800

    New branch

---
+++ FlashCodeCalculatorForPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashCodeCalculatorForPad
+//
+//  Created by Andy on 11/4/15.
+//  Copyright (c) 2015 Mot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
